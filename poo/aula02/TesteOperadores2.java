28e81fb80d1734675cecd353ed109ba6bad608b7
Antonio Carvalho <antoniorcn@hotmail.com>
2024-02-22 17:48:41 -0300

    poo aula02

---
+++ poo/aula02/TesteOperadores2.java
@@ -0,0 +1,9 @@
+public class TesteOperadores2 { 
+    public static void main(String args[]) { 
+        int a = 47;
+        int b = a << 3;
+        System.out.println("B: " + b);
+        int c = a >> 2;
+        System.out.println("C: " + c);
+    }
+}
