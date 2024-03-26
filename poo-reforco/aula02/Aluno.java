a9d7c9a4ef1a01e4aec1d1cf9ef642566cea6106
Antonio Carvalho <antoniorcn@hotmail.com>
2024-03-26 18:53:12 -0300

    poo reforco aula02

---
+++ poo-reforco/aula02/Aluno.java
@@ -0,0 +1,9 @@
+public class Aluno { 
+    public String nome;
+    public String ra;
+    public int idade;
+
+    public Aluno() {
+        
+    }
+}
---
+++ poo-reforco/aula02/TesteAluno.java
@@ -0,0 +1,9 @@
+public class TesteAluno {
+    
+
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        // a1.nome = "Maria Silva";
+        System.out.println("Nome: " + a1.nome);
+    }
+}
