41ada4e5d0d4fa77da81c1836e9fa23d349ceb4d
Antonio Carvalho <antoniorcn@hotmail.com>
2024-03-26 12:32:38 -0300

    nlp aula 06

---
+++ poo-reforco/AlunoTeste.java
@@ -0,0 +1,16 @@
+public class AlunoTeste {
+    public static void main(String[] args) {
+        System.out.println("Executando pela classe Aluno Teste");
+        int j = 20;
+        Aluno joao = new Aluno();
+        Aluno maria = new Aluno();
+
+        joao.ra = "11111";
+        joao.nome = "João Silva";
+        joao.idade = 21;
+
+        maria.ra = "22222";
+        maria.nome = "Maria Silva";
+        maria.idade = 23;
+    }
+}
