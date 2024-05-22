56aa6d2fd7059415ed6f6b286580687a6b02542a
Antonio Carvalho <antoniorcn@hotmail.com>
2024-05-22 11:30:50 -0300

    PDMI Aula 13

---
+++ pdmi/aula13/agenda-contato/app/src/main/java/com/example/agendacontato/activity/ListagemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.agendacontato.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.agendacontato.R
+
+class ListagemActivity: AppCompatActivity() {
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+        setContentView(R.layout.listagem_layout)
+    }
+}
