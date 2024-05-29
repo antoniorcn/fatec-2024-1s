cee463401018f855eb215953d97694ddcc32ef80
Antonio Carvalho <antoniorcn@hotmail.com>
2024-05-29 10:49:31 -0300

    pdmi aula14 parte 3

--- pdmi/aula14/agenda-contato-recyclevioew-firebase/app/src/main/java/com/example/agenda_contato_recyclevioew_firebase/activity/ListagemActivity.kt
+++ pdmi/aula14/agenda-contato-recyclevioew-firebase/app/src/main/java/com/example/agenda_contato_recyclevioew_firebase/activity/ListagemActivity.kt
@@ -3,6 +3,7 @@ package com.example.agenda_contato_recyclevioew_firebase.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.agenda_contato_recyclevioew_firebase.R
 import com.example.agenda_contato_recyclevioew_firebase.model.Contato
 import com.example.agenda_contato_recyclevioew_firebase.recycle.ContatoAdapter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
@@ -17,17 +20,20 @@ import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
 
+
 class ListagemActivity: AppCompatActivity() {
+    val gson = Gson()
     val lista = ArrayList<Contato>()
     var clientHttp = OkHttpClient()
+    lateinit var rcvContatos : RecyclerView
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
         setContentView(R.layout.listagem_layout)
 
-        lista.add(Contato("Joao Silva", "(11) 1111-1111", "joao@teste.com"))
-        lista.add(Contato("Maria Silva", "(11) 2222-2222", "maria@teste.com"))
+//        lista.add(Contato("Joao Silva", "(11) 1111-1111", "joao@teste.com"))
+//        lista.add(Contato("Maria Silva", "(11) 2222-2222", "maria@teste.com"))
 
-        val rcvContatos = findViewById<RecyclerView>(R.id.rcvContatos)
+        rcvContatos = findViewById<RecyclerView>(R.id.rcvContatos)
         val adapter = ContatoAdapter(this, lista)
         rcvContatos.adapter = adapter
         rcvContatos.layoutManager = LinearLayoutManager(this)
@@ -36,6 +42,21 @@ class ListagemActivity: AppCompatActivity() {
         btnFormulario.setOnClickListener {
             val intent = Intent(this, FormularioActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun testeJson() {
+        Log.i("AGENDA-CONTATO", "Dados recebidos convertendo")
+        val corpoTexto = """
+                    {"-NyVYx2BjSlu7piNDOuB":
+	                    {"email":"joao@teste.com","nome":"Joao Silva","telefone":"111111111"},
+                     "-Nz3EaomEb9bilQ4I1pP":
+	                    {"email":"maria@teste.com","nome":"MAria Silva","telefone":"(11) 22222-22222"}}
+                """.trimIndent()
+        val type = object : TypeToken<HashMap<String?, Contato?>?>() {}.type
+        val myMap: HashMap<String, Contato> = gson.fromJson(corpoTexto, type)
+        myMap.values.forEach {
+            Log.i("AGENDA-CONTATO", "Contato: $it")
         }
     }
 
@@ -49,13 +70,23 @@ class ListagemActivity: AppCompatActivity() {
             override fun onFailure(call: Call?, e: IOException?) {
                 Log.e("AGENDA-CONTATO", e?.message.toString())
             }
-
             override fun onResponse(call: Call?, response: Response?) {
+                Log.i("AGENDA-CONTATO", "Dados recebidos convertendo")
                 val body = response?.body()
-                body?.charStream()?.buffered()?.lines()?.forEach {
-                    Log.i("AGENDA-CONTATO", it.toString())
+                val type = object : TypeToken<HashMap<String?, Contato?>?>() {}.type
+                val myMap: HashMap<String, Contato> = gson.fromJson(body?.string(), type)
+                val listaTemp = ArrayList<Contato>()
+                myMap.values.forEach {
+                    Log.i("AGENDA-CONTATO", "Contato: $it")
+                    listaTemp.add(it)
                 }
-
+                this@ListagemActivity.runOnUiThread(
+                    java.lang.Runnable {
+                        lista.clear()
+                        lista.addAll(listaTemp)
+                        rcvContatos.adapter?.notifyDataSetChanged()
+                    }
+                )
             }
         }
         clientHttp.newCall(request).enqueue(response)
